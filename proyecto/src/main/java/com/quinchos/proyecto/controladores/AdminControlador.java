aaba31e33bdf34bf9a872f19ff6de20c8dfcb601
DiegoMendia <diegomendia21@gmail.com>
2024-11-17 20:46:49 -0300

    push de mi rama diego

---
+++ proyecto/src/main/java/com/quinchos/proyecto/controladores/AdminControlador.java
@@ -0,0 +1,17 @@
+package com.quinchos.proyecto.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminControlador {
+
+    @GetMapping("/dashboard")
+    public String panelAdministrativo(){
+        return "administrador";
+    }
+
+
+}
